87a0ab7faf1459dcbc75d3204476216f9778e96d
recepkotan54 <41400042+recepkotan54@users.noreply.github.com>
2018-07-21 11:24:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -292,7 +292,7 @@ client.on('message', msg => {
 });
 
 client.on('guildMemberAdd', member => {
-  const channel = member.guild.channels.find('name', 'hoşgeldin');
+  const channel = member.guild.channels.find('name', '💬genel-sohbet💬');
   if (!channel) return;
   const embed = new Discord.RichEmbed()
   .setColor('RANDOM')
