7a69ad8b8d174765aa4be2d6849fac70dd015a1e
recepkotan54 <41400042+recepkotan54@users.noreply.github.com>
2018-07-21 18:49:52 +0300

    Update davet.js

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -4,16 +4,16 @@ const client = new Discord.Client();
 exports.run = (client, message) => {
   if (message.channel.type !== 'dm') {
     const ozelmesajkontrol = new Discord.RichEmbed()
-    .setColor(0x00AE86)
+    .setColor('RANDOM')
     .setTimestamp()
     .setAuthor(message.author.username, message.author.avatarURL)
-    .setDescription('Özel Mesajlarını Kontrol Et. :postbox:');
+    .setDescription('Özel Mesajlarını Kontrol Et. :inbox_tray:');
     message.channel.sendEmbed(ozelmesajkontrol) }
 	const pingozel = new Discord.RichEmbed()
     .setColor(0x00AE86)
     .setTimestamp()
     .setAuthor(message.author.username, message.author.avatarURL)
-    .setDescription('İşte Davet Linkim: https://discordapp.com/oauth2/authorize?client_id=469820178960482314&permissions=2146958583&scope=bot Buda Bizim Sunucunun :)  https://discord.gg/Ee7hBtV');
+    .setDescription('İşte Davet Linkim: https://discordapp.com/oauth2/authorize?client_id=469820178960482314&permissions=2146958583&scope=bot          Buda Bizim Sunucunun :)  https://discord.gg/Ee7hBtV');
     return message.author.sendEmbed(pingozel)
 };
 
