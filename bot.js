6466ea2f681988109bf4f929182733bcd480df97
recepkotan54 <41400042+recepkotan54@users.noreply.github.com>
2018-07-21 19:27:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -298,7 +298,7 @@ client.on('guildMemberAdd', member => {
   .setColor('RANDOM')
   .setAuthor(member.user.username, member.user.avatarURL)
   .setThumbnail(member.user.avatarURL)
-  .setTitle(':inbox_tray: **Sunucumuza Katıldı** | **Hoşgeldin** ')
+  .setTitle('Sunucumuza Katıldı | Hoşgeldin ')
   .setTimestamp()
   channel.sendEmbed(embed);
 });
