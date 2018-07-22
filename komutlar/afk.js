d3b25d10ff288184c63da6ac0d77e61d8279d8f6
recepkotan54 <41400042+recepkotan54@users.noreply.github.com>
2018-07-22 18:08:37 +0300

    Update afk.js

--- komutlar/afk.js
+++ komutlar/afk.js
@@ -5,7 +5,7 @@ exports.run = (client, message) => {
   message.channel.send({embed: {
                 author: {
                     name: "Afk",
-                    icon_url: "https://i.imgyukle.com/2018/05/07/V49QH.th.png"
+                    icon_url: "https://cdn.discordapp.com/attachments/469843276954992662/470591640696782848/icons8-xbox-r-50.png"
                   },
                 description: "**Artık Afksın (Away From Keyboard) :white_check_mark:**"
               }});
